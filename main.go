e4bf5aa1dd58ec25aa4d494aa79492d544d2dd5f
Johannes Hamfler <jh@z7k.de>
2019-05-06 22:44:22 +0200

    second attempt removing external dependencies

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"crypto/tls"
 	"flag"
 	"fmt"
-	"image"
-	"image/jpeg"
+	//"image"
+	//"image/jpeg"
 	"io"
 	"io/ioutil"
 	"log"
@@ -15,7 +15,7 @@ import (
 	//"path"
 	"regexp"
 	"runtime"
-	"strconv"
+	//"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -796,94 +796,6 @@ func Nausf_UEAuthentication_Authenticate_Request1(w http.ResponseWriter, r *http
 			time.Sleep(100 * time.Millisecond) // fake network latency + parsing time
 		})
 	}
-
-	func newGopherTilesHandler() http.Handler {
-		const gopherURL = "https://blog.golang.org/go-programming-language-turns-two_gophers.jpg"
-		res, err := http.Get(gopherURL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if res.StatusCode != 200 {
-			log.Fatalf("Error fetching %s: %v", gopherURL, res.Status)
-		}
-		slurp, err := ioutil.ReadAll(res.Body)
-		res.Body.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-		im, err := jpeg.Decode(bytes.NewReader(slurp))
-		if err != nil {
-			if len(slurp) > 1024 {
-				slurp = slurp[:1024]
-			}
-			log.Fatalf("Failed to decode gopher image: %v (got %q)", err, slurp)
-		}
-		type subImager interface {
-			SubImage(image.Rectangle) image.Image
-		}
-		const tileSize = 32
-		xt := im.Bounds().Max.X / tileSize
-		yt := im.Bounds().Max.Y / tileSize
-		var tile [][][]byte // y -> x -> jpeg bytes
-		for yi := 0; yi < yt; yi++ {
-			var row [][]byte
-			for xi := 0; xi < xt; xi++ {
-				si := im.(subImager).SubImage(image.Rectangle{
-					Min: image.Point{xi * tileSize, yi * tileSize},
-					Max: image.Point{(xi + 1) * tileSize, (yi + 1) * tileSize},
-				})
-				buf := new(bytes.Buffer)
-				if err := jpeg.Encode(buf, si, &jpeg.Options{Quality: 90}); err != nil {
-					log.Fatal(err)
-				}
-				row = append(row, buf.Bytes())
-			}
-			tile = append(tile, row)
-		}
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ms, _ := strconv.Atoi(r.FormValue("latency"))
-			const nanosPerMilli = 1e6
-			if r.FormValue("x") != "" {
-				x, _ := strconv.Atoi(r.FormValue("x"))
-				y, _ := strconv.Atoi(r.FormValue("y"))
-				if ms <= 1000 {
-					time.Sleep(time.Duration(ms) * nanosPerMilli)
-				}
-				if x >= 0 && x < xt && y >= 0 && y < yt {
-					http.ServeContent(w, r, "", time.Time{}, bytes.NewReader(tile[y][x]))
-					return
-				}
-			}
-			io.WriteString(w, "<html><body onload='showtimes()'>")
-			fmt.Fprintf(w, "A grid of %d tiled images is below. Compare:<p>", xt*yt)
-			for _, ms := range []int{0, 30, 200, 1000} {
-				d := time.Duration(ms) * nanosPerMilli
-				fmt.Fprintf(w, "[<a href='https://%s/gophertiles?latency=%d'>HTTP/2, %v latency</a>] [<a href='%s/gophertiles?latency=%d'>HTTP/1, %v latency</a>]<br>\n",
-				httpsHost(), ms, d,
-				http1Prefix(), ms, d,
-			)
-		}
-		io.WriteString(w, "<p>\n")
-		cacheBust := time.Now().UnixNano()
-		for y := 0; y < yt; y++ {
-			for x := 0; x < xt; x++ {
-				fmt.Fprintf(w, "<img width=%d height=%d src='/gophertiles?x=%d&y=%d&cachebust=%d&latency=%d'>",
-				tileSize, tileSize, x, y, cacheBust, ms)
-			}
-			io.WriteString(w, "<br/>\n")
-		}
-		io.WriteString(w, `<p><div id='loadtimes'></div></p>
-		<script>
-		function showtimes() {
-			var times = 'Times from connection start:<br>'
-			times += 'DOM loaded: ' + (window.performance.timing.domContentLoadedEventEnd - window.performance.timing.connectStart) + 'ms<br>'
-			times += 'DOM complete (images loaded): ' + (window.performance.timing.domComplete - window.performance.timing.connectStart) + 'ms<br>'
-			document.getElementById('loadtimes').innerHTML = times
-		}
-		</script>
-		<hr><a href='/'>&lt;&lt Back to Go HTTP/2 demo server</a></body></html>`)
-	})
-}
 
 func httpsHost() string {
 	if *hostHTTPS != "" {
